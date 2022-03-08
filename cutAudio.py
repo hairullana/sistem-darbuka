5e5dfb8b0d2459bc91ac2ee447fd5fb85369afc8
Hairul Lana <hairullana99@gmail.com>
2022-03-08 14:29:24 +0800

    update basic ipynb

--- cutAudio.py
+++
@@ -1,4 +0,0 @@
-from pydub import AudioSegment
-newAudio = AudioSegment.from_wav("d:/ngoding/sistem darbuka/dataset/tonePattern/baladi2_5.wav")
-newAudio = newAudio[1000:2000]
-# newAudio.export('newSong.wav', format="wav")
